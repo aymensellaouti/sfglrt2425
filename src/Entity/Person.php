a0e81501c49b21bf491a55802ffb0ea90450cf79
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-24 09:06:07 +0100

    prepare fixtures and trait

--- src/Entity/Person.php
+++ src/Entity/Person.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\PersonRepository;
+use App\Traits\TimeStampTrait;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
--- src/Traits/TimeStampTrait.php
+++ src/Traits/TimeStampTrait.php
@@ -1,8 +1,21 @@
 <?php
 
 namespace App\Traits;
-
+use Doctrine\ORM\Mapping as ORM;
 trait TimeStampTrait
 {
+    #[ORM\Column(type:"datetime", nullable:true)]
+    private \DateTime $createdAt;
 
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTime $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
 }
