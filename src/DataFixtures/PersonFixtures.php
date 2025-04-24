eae486711b37ec69634b4297344e626102572ca3
aymensellaouti <aymen.sellaouti@gmail.com>
2025-04-24 09:02:45 +0100

    prepare fixtures and trait/**

---
+++ src/DataFixtures/PersonFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Person;
+use App\Entity\Skill;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class PersonFixtures extends Fixture  implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $fakerAr = Factory::create('ar_SA');
+        $fakerFr = Factory::create('fr_FR');
+        $skills = $manager->getRepository(Skill::class)->findAll();
+        //$this->getReference(SkillFixture::SKILLS, SkillFixture::class)->skills;
+        for ($i=0 ; $i<10 ; $i++) {
+            $faker = $i%2 ? $fakerAr : $fakerFr;
+            $person = new Person();
+            shuffle($skills);
+            for ($j=0 ; $j< 3 ; $j++) {
+                $person->addSkill($skills[$j]);
+            }
+            $person->setAge($faker->numberBetween($min = 18, $max = 60));
+            $person->setName($faker->name());
+
+            $manager->persist($person);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array {
+        return [SkillFixture::class];
+    }
+}
---
+++ src/DataFixtures/SkillFixture.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Skill;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class SkillFixture extends Fixture
+{
+    const SKILLS = 'skills';
+    public function load(ObjectManager $manager): void
+    {
+        $skills = [
+            ["name" => "HTML/CSS", "type" => "Technique", "level" => "Avancé"],
+            ["name" => "JavaScript", "type" => "Technique", "level" => "Intermédiaire"],
+            ["name" => "PHP", "type" => "Technique", "level" => "Avancé"],
+            ["name" => "SQL/MySQL", "type" => "Technique", "level" => "Intermédiaire"],
+            ["name" => "Git & GitHub", "type" => "Technique", "level" => "Avancé"],
+            ["name" => "Communication orale", "type" => "Soft Skill", "level" => "Avancé"],
+            ["name" => "Travail en équipe", "type" => "Soft Skill", "level" => "Avancé"],
+            ["name" => "Résolution de problèmes", "type" => "Soft Skill", "level" => "Intermédiaire"],
+            ["name" => "Gestion du temps", "type" => "Soft Skill", "level" => "Intermédiaire"],
+            ["name" => "Anglais professionnel", "type" => "Langue", "level" => "Avancé"],
+            ["name" => "Français", "type" => "Langue", "level" => "Langue maternelle"],
+            ["name" => "Espagnol", "type" => "Langue", "level" => "Débutant"],
+            ["name" => "Leadership", "type" => "Soft Skill", "level" => "Intermédiaire"],
+            ["name" => "Python", "type" => "Technique", "level" => "Débutant"],
+            ["name" => "Gestion de projet", "type" => "Gestion", "level" => "Intermédiaire"],
+            ["name" => "Scrum / Agile", "type" => "Gestion", "level" => "Intermédiaire"],
+            ["name" => "Empathie", "type" => "Soft Skill", "level" => "Avancé"],
+            ["name" => "WordPress", "type" => "Technique", "level" => "Intermédiaire"],
+            ["name" => "Créativité", "type" => "Soft Skill", "level" => "Avancé"],
+            ["name" => "Conception UI/UX", "type" => "Technique", "level" => "Débutant"]
+        ];
+        $generatedSkills = [];
+        foreach ($skills as $skillElement) {
+            $skill = new Skill();
+            $skill->setDesignation($skillElement["name"]);
+            $manager->persist($skill);
+            $generatedSkills[] = $skill;
+        }
+//        $shared = new \stdClass();
+//        $shared->skills = $generatedSkills;
+//        $this->addReference(self::SKILLS, $shared);
+        $manager->flush();
+    }
+}
